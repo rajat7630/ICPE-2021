5fb90bed7ea60437d36a72b19769ca3a380a82a1
Rajat Garg <rajatgarg7630@gmail.com>
2020-09-18 14:33:18 +0530

    feat:keynote design improved

--- src/Components/Keynote/index.js
+++ src/Components/Keynote/index.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import Navbar from '../Navbar/index';
-
+import "./keynote.css";
 class KeyNote extends Component {
     render() {
         return (
             <>
-                <Navbar />
-                <h1>
-                 KeyNote
-                </h1>
+                <div className="w-screen h-screen">
+                    <div className="w-screen h-screen absolute">
+                        <div className="w-screen h-screen bg-cover absolute keynoteaddfilter"></div>
+                    </div>
+                    <div className="w-screen h-screen absolute">
+                        <div className="fixed w-screen navbar">
+                            <Navbar />
+                        </div>
+                        <div className="homeBlock mx-auto text-center text-white">
+                            <h1 className="mx-auto eventHeading font-bold">
+                                KEYNOTE
+                        </h1>
+                        </div>
+                    </div>
+                </div>
             </>
         );
     }
