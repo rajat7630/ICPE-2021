3da5467a342f9b8981e25244eddf89fe63a4e53a
Karunam Goyal <karunam.goyal25@gmail.com>
2020-09-19 12:38:42 +0530

    Contact and minor navbar Modification

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import 'materialize-css';
 import { Navbar as Navigation, NavItem, Icon } from "react-materialize";
 import MenuIcon from '@material-ui/icons/Menu';
+import Logo from './logos/combined3.png';
+import "./index.css"
 class Navbar extends Component {
     render() {
         return (
-            <Navigation className="transparent sticky"
+            <Navigation className="white"
                 alignLinks="right"
-                brand={<a className="brand-logo" href="#">Logo</a>}
+                brand={<a className="brand-logo brand-margin" href='/'><img className="logo-size-nit" src={Logo}/></a>}
                 id="mobile-nav"
+                fixed={true}
                 menuIcon={<MenuIcon color="secondary"/>}
                 options={{
                     draggable: true,
@@ -21,12 +24,12 @@ class Navbar extends Component {
                     outDuration: 200,
                     preventScrolling: true
                 }}>
-                <NavItem className="white-text" href="/">HOME</NavItem>
-                <NavItem className="white-text" href="/committee">COMMITTEE</NavItem>
-                <NavItem className="white-text" href="/keynote">KEYNOTE</NavItem>
-                <NavItem className="white-text" href="/authors">FOR AUTHORS</NavItem>
-                <NavItem className="white-text" href="/dates">KEY DATES</NavItem>
-                <NavItem className="white-text" href="/contactus">CONTACT US</NavItem>
+                <NavItem className="black-text" href="/">HOME</NavItem>
+                <NavItem className="black-text" href="/committee">COMMITTEE</NavItem>
+                <NavItem className="black-text" href="/keynote">KEYNOTE</NavItem>
+                <NavItem className="black-text" href="/authors">FOR AUTHORS</NavItem>
+                <NavItem className="black-text" href="/dates">KEY DATES</NavItem>
+                <NavItem className="black-text" href="/contactus">CONTACT US</NavItem>
             </Navigation>
         );
     }
